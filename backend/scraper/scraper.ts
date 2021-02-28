85bd9d54d4e3ae0bd43850e641a58eb0df786b81
Jared Min <jkymin2@gmail.com>
2021-02-28 16:21:11 -0500

    Update database schema and data scraper Add API routes to get all providers and all boston area providers

--- backend/scraper/scraper.ts
+++ backend/scraper/scraper.ts
@@ -5,11 +5,13 @@ const axios = require('axios')
 
 const url="https://api.us.castlighthealth.com/vaccine-finder/v1/provider-locations/search?medicationGuids=779bfe52-0dd8-4023-a183-457eb100fccc,a84fb9ed-deb4-461c-b785-e17c782ef88b&lat=42.35&long=-71.1&radius=50"
 
+const bostonZipCodes = ["02128", "02126", "02136", "02122", "02124", "02121", "02125", "02131", "02119", "02120", "02132", "02111", "02118", "02130", "02127", "02210", "02163", "02134", "02135", "02129", "02108", "02114", "02116", "02199", "02222", "02109", "02110", "02113", "02115", "02215"]
+
 
 async function scrape() {
     const html = await axios.get(url)
     const providerData = html.data.providers
-    // console.log(providerData)
+    console.log(providerData)
     const data = []
 
     providerData.forEach(provider => {
@@ -18,21 +20,36 @@ async function scrape() {
     return data;
 }
 
+function isBostonProvider(zip){ return (bostonZipCodes.includes(zip))}
 
 let res = scrape()
-    .then((t) =>  {
-        // const p = t
-        t.forEach( d => {
-            axios.post("http://localhost:5000/vaccineCenter", {
-                location: d.name,
-                address1: d.address1,
-                address2: d.address2,
-                city: d.city,
-                state: d.state,
-                zipCode: d.zip,
-                phone: d.phone,
-                inStock: d.in_stock
+    .then((pd) =>  {
+        pd.forEach( (p) => {
+            const bostonProvider = isBostonProvider(p.zip)
+            console.log(bostonProvider)
+            axios.post("http://localhost:5000/vaccineProvider", {
+                location: p.name,
+                address1: p.address1,
+                address2: p.address2,
+                city: p.city,
+                state: p.state,
+                zip: p.zip,
+                phone: p.phone,
+                inStock: p.in_stock,
+                bostonProvider: bostonProvider,
+                availabilityLink: "none",
+                googleMapsLink: "none",
+                providerWebpage: "none",
+                vaccinesOffered: "none",
+                firstDoseAvailability: false,
+                secondDoseAvailability: false
             })
+            .catch((err) => {
+                console.log(err.message);
+            });
         })
     })
+
+
+
 
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -8,39 +8,83 @@ const port = 5000
 
 app.use(express.json())
 
-// app.get('/vaccineCenters')
-
-// app.get('/', async (req, res) => {
-//     console.log("Server is running ya fool")
-//     res.end()
-// })
-
-app.post('/vaccineCenter', async (req, res) => {
-    const { location, address1, address2, city, state, zipCode, phone, inStock } = req.body;
-    const result = await prisma.vaccineCenter.create({
-        data: {
-            location,
-            address1, 
-            address2,
-            city,
-            state,
-            zipCode,
-            phone,
-            inStock
-        }
-    });
-    console.log("data added");
-    res.json(result);
+app.post('/vaccineProvider', async (req, res) => {
+  const { location, address1, address2, city, state, zip, phone, bostonProvider,  availabilityLink, googleMapsLink, providerWebpage, inStock, vaccinesOffered, firstDoseAvailability, secondDoseAvailability } = req.body;
+  const result = await prisma.vaccineProvider.create({
+      data: {
+          location,
+          address1, 
+          address2,
+          city,
+          state,
+          zip,
+          phone,
+          bostonProvider,
+          availabilityLink, 
+          googleMapsLink, 
+          providerWebpage, 
+          inStock,
+          vaccinesOffered, 
+          firstDoseAvailability, 
+          secondDoseAvailability
+      },
+  });
+  // console.log("data added");
+  res.json(result);
 })
 
-app.get('/vaccineCenters', async (req, res) => {
-    const vaccineCenters = await prisma.vaccineCenter.findFirst()
-    console.log(vaccineCenters)
-    res.json(vaccineCenters)
+// Get all vaccine providers
+app.get('/vaccineProviders', async (req, res) => {
+  const vaccineProviders = await prisma.vaccineProvider.findMany()
+  console.log(vaccineProviders)
+  res.json(vaccineProviders)
+})
+
+// Get all Boston area vaccine providers
+app.get('/bostonVaccineProviders', async (req, res) => {
+  const bostonVaccineProviders = await prisma.vaccineProvider.findMany({
+    where: {
+      bostonProvider: true,
+    }
+  })
+  console.log(bostonVaccineProviders)
+  res.json(bostonVaccineProviders);
 })
 
 const server = app.listen(port, () =>
-  console.log(
-    `🚀 Server ready at: http://localhost:${port}`,
-  ),
+console.log(
+  `🚀 Server ready at: http://localhost:${port}`,
+),
 )
+
+
+// app.put('/vaccineProvider', (req, res) {
+//   const { location, address1, address2, city, state, zip, phone, inStock, availabilityLink, googleMapsLink, providerWebpage, vaccinesOffered, firstDoseAvailability, secondDoseAvailability } = req.body;
+
+//   const result = await prisma.vaccineProvider.update({
+
+//   })
+//   res.send({
+//     data: {
+//             location,
+//             address1, 
+//             address2,
+//             city,
+//             state,
+//             zip,
+//             phone,
+//             inStock,
+//             availabilityLink, 
+//             googleMapsLink, 
+//             providerWebpage, 
+//             vaccinesOffered, 
+//             firstDoseAvailability, 
+//             secondDoseAvailability
+//     },
+//   })
+// })
+// app.delete('/vaccineProvider', async (req, res) => {
+//     const deleted = await prisma.vaccineProvider.deleteMany()
+//     res.json(deleted);
+// })
+
